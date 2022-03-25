876d0ca51d43542c13de2d34a4c99f11f90ddf76
Ariel <cervanteslibch@gmail.com>
2022-03-25 00:58:18 -0300

    Agregue al mock de ItemListContainer  un catch de error y un finally incluyendo cargando

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,11 +4,15 @@ import {getProducts} from '../dataBase/dataProductos'
 
 const ItemListContainer = ({greetings}) => {
     const [productos, setProductos] = useState([])
+    const [cargando, setCargando] = useState(true)
 
     //Si no usamos useEffect va a llamarse primero (antes que el render) y va a bloquear el rendering en caso de tardios
     //Con el array vacio llamamos una sola vez a la API despues del renderizado o montado del componente
     useEffect(() => {
-        getProducts.then(resp => setProductos(resp))
+        getProducts
+        .then(resp => setProductos(resp))
+        .catch(err => alert("Hubo un error"))
+        .finally(() => setCargando(false))
     }, [])
 
 
@@ -16,8 +20,10 @@ const ItemListContainer = ({greetings}) => {
         <div>
             <h2>Bienvenidos a Librería Cervantes</h2>
              {greetings}  
+             {cargando ? <h2>Cargando...</h2> 
+             :
              <ItemList productos={productos}/> 
-            
+            }
         </div>
         
      );
