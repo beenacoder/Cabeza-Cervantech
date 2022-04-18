b17107808d2544c975c4d3df7cb4c2ce516b8270
Ariel <cervanteslibch@gmail.com>
2022-04-17 21:40:49 -0300

    Agregue condicionales para el rendering del Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,17 +6,28 @@ const Cart = () => {
     const {cartList, emptyCart, totalPrice, itemQnty} = useCartContext()
 
     return (
-        <div>
+        <div className="cart-container">
             <h1>Cart</h1>
-            {cartList.map(item => <CartItem  key={item.id} id= {item.id} title={item.title} price={item.price} cantidad={item.cantidad} />
+            {cartList.length === 0 ?
+                <>
+                    <h2>No tienes ningun producto agregado</h2>
+                    <Link to = '/'>
+                        <button>Llevame al catalogo</button>
+                    </Link>
+                </>
+             : 
+                <>
+                {cartList.map(item => <CartItem  key={item.id} id= {item.id} title={item.title} price={item.price} cantidad={item.cantidad} />
                                     )}
-            <div>Cantidad total de productos: {itemQnty()}</div>
-            <div>Total a pagar: $ {totalPrice()}</div>                        
-            <button onClick={emptyCart}>Vaciar Carrito</button>
-            <span>-</span>
-            <Link to = '/'>
-            <button>Seguir Comprando</button>
-            </Link>
+                    <div>Cantidad total de productos: {itemQnty()}</div>
+                    <div>Total a pagar: $ {totalPrice()}</div>                        
+                    <button onClick={emptyCart}>Vaciar Carrito</button>
+                    <span>-</span>
+                    <Link to = '/'>
+                    <button>Seguir Comprando</button>
+                    </Link>
+                </>
+}
         </div>
     )
 }
