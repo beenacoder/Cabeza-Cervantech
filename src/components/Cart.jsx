e601675e9809ee06989bbb8fbd6fe159c47fe2c7
Ariel <cervanteslibch@gmail.com>
2022-04-18 23:38:02 -0300

    Conecte Firebase para escrituras, agregue para generar la orden para el usuario

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,50 @@
 import { Link } from "react-router-dom"
 import { useCartContext } from "../context/CartContext"
 import CartItem from "./CartItem"
+import { getFirestore, collection, addDoc  } from 'firebase/firestore'
+
 
 const Cart = () => {
     const {cartList, emptyCart, totalPrice, itemQnty} = useCartContext()
+
+
+    const generateOrder = (e) => {
+        e.preventDefault();
+
+        let order = {}
+
+        order.buyer = {name: 'Ariel', email: 'ariel@gmail.com', phone: '0303456'}
+        order.total = totalPrice()
+
+        order.items = cartList.map(cartItem => {
+            const id = cartItem.id
+            const nombre = cartItem.title
+            const precio = cartItem.price * cartItem.cantidad
+
+            return {id, nombre, precio}
+        })
+        // console.log(order)
+
+        //Creacion de un documento
+        const dataBase = getFirestore()
+        const queryCollection = collection(dataBase, 'orders')
+        addDoc(queryCollection, order)
+        .then(resp => console.log(resp))
+        .catch(err => "Hubo un error en la pagina")
+        .finally(emptyCart)
+
+        //Update, Modificar un archivo
+        // const queryUpdate = doc(dataBase, 'productos', '0R4DRUodJsIDYdYdTNIe')
+        // updateDoc(queryUpdate, {
+        //     stock: 2
+        // })
+
+        
+
+
+    }
+
+
 
     return (
         <div className="cart-container">
@@ -26,6 +67,8 @@ const Cart = () => {
                     <Link to = '/'>
                     <button>Seguir Comprando</button>
                     </Link>
+                    <span>-</span>
+                    <button onClick={generateOrder}>Finalizar Compra</button>
                 </>
 }
         </div>
